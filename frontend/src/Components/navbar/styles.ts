2b0532b62a97fea097869fe7627d498a7f870da2
victor <vefg@cin.ufpe.br>
2022-11-11 09:44:26 -0300

    added a little to navbar style

--- frontend/src/Components/navbar/styles.ts
+++ frontend/src/Components/navbar/styles.ts
@@ -4,9 +4,15 @@ import { Link } from 'react-router-dom';
 export const Nav = styled.nav`
 	display: flex;
 	justify-content: space-between;
+	align-items: center;
+
+	background-color: #e5e5e5;
+	margin: 0;
 `;
 
-export const Dashboard = styled(Link)``;
+export const Dashboard = styled(Link)`
+	display: flex;
+`;
 
 export const Logo = styled.img`
 	max-height: 50px;
