a05cbc20cce474ed7707f917ecad7b9eaeb0d468
Gaudiot <victor.gaudiot@gmail.com>
2023-02-06 11:17:11 -0300

    Medal only created with existent user

--- Backend/src/modules/medals/services/CreateMedalService.ts
+++ Backend/src/modules/medals/services/CreateMedalService.ts
@@ -1,4 +1,6 @@
 import { inject, injectable } from 'tsyringe';
+import AppError from '../../../shared/errors/AppError';
+import IUserRepository from '../../users/repositories/interfaces/IUserRepository';
 import ICreateMedalDTO from '../dtos/ICreateMedalDTO';
 import Medal from '../entities/Medal';
 import IMedalRepository from '../repositories/interfaces/IMedalRepository';
@@ -8,9 +10,18 @@ class CreateMedalService {
 	constructor(
 		@inject('MedalsRepository')
 		private medalsRepository: IMedalRepository,
+
+		@inject('UsersRepository')
+		private usersRepository: IUserRepository,
 	) {}
 
 	public async execute(createMedalDTO: ICreateMedalDTO): Promise<Medal>{
+		const userExists = await this.usersRepository.findById(createMedalDTO.medalist_id);
+
+		if(userExists){
+			throw new AppError('contestant does not exists', 404);
+		}
+
 		const medal = this.medalsRepository.create(createMedalDTO);
 
 		return medal;
