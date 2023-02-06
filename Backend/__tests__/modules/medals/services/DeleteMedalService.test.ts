6cd932ae5f62f6324c4fdf890968aa11df4d0f53
Gaudiot <victor.gaudiot@gmail.com>
2023-02-06 17:53:21 -0300

    delete medal tests

---
+++ Backend/__tests__/modules/medals/services/DeleteMedalService.test.ts
@@ -0,0 +1,43 @@
+import DeleteMedalService from '../../../../src/modules/medals/services/DeleteMedalService';
+
+import FakeMedalsRepository from '../../../../src/modules/medals/repositories/FakeMedalsRepository';
+
+import IUser from '../../../../src/modules/users/entities/interfaces/IUser';
+import AppError from '../../../../src/shared/errors/AppError';
+
+let fakeMedalsRepository: FakeMedalsRepository;
+
+let deleteMedalService: DeleteMedalService;
+
+describe('Delete Medal', () => {
+	beforeEach(() => {
+		fakeMedalsRepository = new FakeMedalsRepository();
+
+		deleteMedalService = new DeleteMedalService(
+			fakeMedalsRepository
+		);
+	});
+
+	it('Should delete medal', async () => {
+		const medal = await fakeMedalsRepository.create({
+			contest_date: new Date(),
+			contest_name: 'Contest #1',
+			medalist_id: '123',
+			position: 'gold'
+		});
+
+		await deleteMedalService.execute({
+			medal_id: medal.id
+		});
+
+		const medalExists = await fakeMedalsRepository.findById(medal.id);
+
+		expect(medalExists).toBeUndefined();		
+	});
+
+	it('Should not be able to delete unexisting medal', async () => {
+		await expect(deleteMedalService.execute({
+			medal_id: '123'
+		})).rejects.toBeInstanceOf(AppError);
+	});
+});
