2fc123635c64f8e75c4f49781b8246a70e62f323
Gaudiot <victor.gaudiot@gmail.com>
2023-02-10 20:46:54 -0300

    removed default image

--- frontend/src/Pages/Profile/index.tsx
+++ frontend/src/Pages/Profile/index.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import api from '../../services/api';
-import ballon from '../../Images/defaultProfileImage.png';
 import { Container, Content, Info } from './styles';
 
 interface ProfileData {
